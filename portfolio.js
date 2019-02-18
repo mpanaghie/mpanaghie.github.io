8397c43b8d965b33886b7f153fe3315cd8dbab3c
MadalinaPanaghie <madalina.panaghie@gmail.com>
2019-02-18 22:22:51 +0200

    Add images at high resolution. Add on click resize event.

--- portfolio.js
+++ portfolio.js
@@ -1,14 +1,16 @@
 
-function addImages (className, book, count) {
+function addImages (className, book, count, startIndex) {
     var portfolioContainer = document.getElementsByClassName(className)[0];
 
-    for (var i=1;i<=count;i++) {
+    for (var i=startIndex;i<startIndex + count;i++) {
         var div = document.createElement("DIV");
         div.setAttribute("class", "portfolio-item-wrapper");
     
         var image = document.createElement("IMG");
-        image.setAttribute("src", `res/img/${book}/Page_${i}.jpg`);
-        image.setAttribute("class", "portfolio-item");
+        image.setAttribute("src", `res/img/${book}/thumb/Page_${i} copy.jpg`);
+        image.setAttribute("class", 'portfolio-item');
+        image.setAttribute("id", `image-${i}`);
+        image.setAttribute("onclick", `rescale('${book}', '${i}')`);
     
         div.appendChild(image);
     
@@ -16,5 +18,15 @@ function addImages (className, book, count) {
     }
 }
 
-addImages("book1", "Book1", 54);
-addImages("book2", "Book2", 18);
+function rescale(book, page) {
+    var image = document.getElementById(`image-${page}`);
+    image.setAttribute("src", `res/img/${book}/high/Page_${page} copy.jpg`);
+    if(image.getAttribute("class") === "portfolio-item") {
+        image.setAttribute("class", 'portfolio-item-big');
+    } else {
+        image.setAttribute("class", 'portfolio-item');
+    }
+}
+
+addImages("book1", "Book1", 54, 1);
+addImages("book2", "Book2", 18, 55);
